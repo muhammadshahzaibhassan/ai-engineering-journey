81229ff0810fcb90f1b6bd2d67099dfa0125a484
Muhammad Shahzaib <i.m.shahzaib.243@gmail.com>
2026-06-19 22:30:44 +0500

    Day 2 code files

---
+++ Learning-Python/day-2/inputs.py
@@ -0,0 +1,7 @@
+# input() = a function that prompts the user to entr data, Returns the entered data as a string.
+
+# input("what is your name? ") or name = input("what is your name? ")
+age = int(input("what is your age? "))
+age2 = age + 1
+print(f"Hello, you are {age} years old and next year you will be {age2} years old!")
+
