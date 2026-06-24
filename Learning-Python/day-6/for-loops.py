8c6ae027f0288f37c1472ab3f77665e84e04ceb7
Muhammad Shahzaib <i.m.shahzaib.243@gmail.com>
2026-06-24 22:30:37 +0500

    add for loop learning

---
+++ Learning-Python/day-6/for-loops.py
@@ -0,0 +1,30 @@
+# for loops = execute a block of code a fixed number of times.
+#             You can iterate over a range, string, sequence, etc.
+
+
+
+#lesson 1
+# for x in range(1, 11, 3):
+#     print(x)
+
+# print("HAPPY NEW YEAR")
+
+
+
+
+# #Lesson2
+# credit_card = "1234-5678-9012-3456"
+# for x in credit_card:
+#     print(x)
+
+
+
+#lesson3 continue and break
+for x in range(1, 21):
+    if x == 13:
+        continue # to skip iteration.
+    else: 
+        print(x)
+
+
+          
