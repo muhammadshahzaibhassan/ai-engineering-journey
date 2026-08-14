81cb5f379fd9d9d03ec96957d0a0de019a4658d1
Muhammad Shahzaib <i.m.shahzaib.243@gmail.com>
2026-08-14 12:19:20 +0500

    Add train.py modified and corrected

--- Machine-Learning-Stuff/Decision-Tree/train.py
+++ Machine-Learning-Stuff/Decision-Tree/train.py
@@ -3,19 +3,38 @@ from sklearn.model_selection import train_test_split
 import numpy as np
 from DecisionTree import DecisionTree
 
+
+# Load dataset
 data = datasets.load_breast_cancer()
+
 X, y = data.data, data.target
 
+
+# Split dataset
 X_train, X_test, y_train, y_test = train_test_split(
-    X, y, test_size = 0.2, random_state = 1234
+    X,
+    y,
+    test_size=0.2,
+    random_state=1234
 )
 
+
+# Create model
 clf = DecisionTree()
+
+# Train model
 clf.fit(X_train, y_train)
+
+# Make predictions
 predictions = clf.predict(X_test)
 
+
+# Accuracy function
 def accuracy(y_test, y_pred):
     return np.sum(y_test == y_pred) / len(y_test)
 
+
+# Calculate accuracy
 acc = accuracy(y_test, predictions)
+
 print(acc)
