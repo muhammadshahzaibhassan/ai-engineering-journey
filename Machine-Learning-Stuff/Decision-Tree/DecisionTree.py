36806d88b7a7ea26788af7d923203017416dacdf
Muhammad Shahzaib <i.m.shahzaib.243@gmail.com>
2026-08-14 09:26:30 +0500

    Halfway through decision trees

--- Machine-Learning-Stuff/Decision-Tree/DecisionTree.py
+++ Machine-Learning-Stuff/Decision-Tree/DecisionTree.py
@@ -1,3 +1,8 @@
+import numpy as np
+from collections import Counter 
+
+
+
 class Node:
     def __init__(self, feature=None, threshold=None, left=None, right=None, *, value=None):
         self.feature = feature
@@ -23,5 +28,86 @@ class DecisionTree:
         self.n_features = X.shape[1] if not self.n_features else min(X.shape[1, self.n_features])
         self.root = self._grow_tree(X, y)
 
+    def _grow_tree(self, X, y, depth = 0):
+        n_samples, n_feats = X.shapes
+        n_labels = len(np.unique(y))
+
+
+        # check the stopping criteria
+        if (depth >= self.max_depth or n_labels == 1 or n_samples<self.min_samples_split):
+            leaf_value = self._most_common_label(y)
+            return Node(value=leaf_value)
+        feat_idxs = np.random.choice(self.n_feats, self.n_features, replace=False)
+
+
+        # find the best split
+        best_features, best_thresh = self._best_split(X, y, feat_idxs)
+
+
+        # create child nodes
+
+
+    def _best_split(self, X, y, feat_idxs):
+        best_gain = -1
+        split_idx, split_threshold = None, None
+
+        for feat_idx, in feat_idxs:
+            X_column = X[:, feat_idx]
+            thresholds = np.unique(X_column)
+
+            for thr in thresholds:
+                # calculate the information gain
+                gain = self._information_gain(y, X_column, thr)
+
+
+                if gain > best_gain:
+                    best_gain = gain
+                    split_idx = feat_idx
+                    split_threshold = thr
+
+        return split_idx, split_threshold
+
+    def _information_gain(self ,y, X_column, thr):
+        # parent entropy
+        parent_entropy = self._entropy(y)
+         
+
+
+
+        # create children
+        left_idx, right_idx = self._split(X_column, threshold)
+
+
+
+        # calculate the weighted avg. entropy of children
+
+
+
+        # calculate the IG
+
+
+    def _split(self, X_column, split_thresh):
+        left_idxs = np.argwhere() 
+        right_idxs
+
+
+
+
+    def _entropy(self, y):
+        hist = np.bincount(y)
+        ps = hist / len(y)
+        return -np.sum[(p * np.log(p) for p in ps if p > 0)]
+
+
+
+
+    def _most_common_label(self, y):
+        counter = Counter(y)
+        value = counter.most_common(1)[0][0]
+        return value
+
+
+
+    def predict():
 
         
