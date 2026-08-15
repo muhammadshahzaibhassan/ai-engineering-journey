eede6958692f6b221c3cce68f70a3f928628a14c
Muhammad Shahzaib <i.m.shahzaib.243@gmail.com>
2026-08-15 11:51:12 +0500

    Add Iterating-through-Files-lec-2.py Completed!

--- Python/File-Handling/Iterating-through-Files-Lec-2.py
+++ Python/File-Handling/Iterating-through-Files-Lec-2.py
@@ -2,9 +2,27 @@ with open("Python\File-Handling\spider.txt") as file:
     for line in file:
         print(line.upper()) 
 # ⬆️ this will print the file, in uppercase, but in output there is a newline character.
+print("")
+print("==================================================================================")
+print("")
+
 
 with open("Python\File-Handling\spider.txt") as file:
     for line in file:
         print(line.strip().upper()) 
 # ⬆️ this will print the file, in uppercase, but newline character is removed by the help of strip function.
+print("")
+print("==================================================================================")
+print("")
 
+
+file = open("Python\File-Handling\spider.txt")
+lines = file.readlines()
+file.close()
+lines.sort()
+print(lines)
+# ⬆️ the lines have been sorted alphabetically, so they're no longer in ​the order that they were in the file. ​Second, we can see that Python displays a newline character using "\n" symbol ​when printing a list of strings.
+
+print("")
+print("==================================================================================")
+print("")
