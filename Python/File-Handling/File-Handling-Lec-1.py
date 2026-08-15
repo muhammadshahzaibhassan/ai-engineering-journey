517d893453a26edce64e9a21413a8d31add71dcc
Muhammad Shahzaib <i.m.shahzaib.243@gmail.com>
2026-08-15 09:51:49 +0500

    Add File-Handling-Lec-1 Completed!

--- Python/File-Handling/File-Handling-Lec-1.py
+++ Python/File-Handling/File-Handling-Lec-1.py
@@ -1,7 +1,6 @@
-file = open("spider.txt")
-print(file.readline())
-print(file.readline())
-print(file.read())
-file.close()
-with open("spider.txt") as file:
+file = open("Python\File-Handling\spider.txt") # Let us open the file using paths
+print(file.readline()) # Let us the read the single line of the file
+print(file.read()) # Let us reatd the complete file
+file.close()  # Let us close the file. 
+with open("spider.txt") as file: # By using "with" block, python will close file automatically so we don't have to remember to close file
     print(file.readline())
